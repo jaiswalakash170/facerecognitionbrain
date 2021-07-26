d39786be860c780924abfb7a5fbe6f30ad4d6998
Akash Jaiswal <akashjaiswal@iiitdmj.ac.in>
2021-07-26 23:59:22 +0530

    Wrapped our Logo inside Tilt component from react-parallax-tilt package

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,9 +1,14 @@
 import react from 'react';
+import Tilt from 'react-parallax-tilt';
 
 const Logo = () => {
     return (
         <div className="ma4 mt0">
-
+            <Tilt className="Tilt" style={{ height: '150px', width: '150px', backgroundColor: 'darkgreen' }}>
+                <div className="Tilt-inner">
+                    <h1>React Parallax Tilt 👀</h1>
+                </div>
+            </Tilt>
         </div>
     );
 }
