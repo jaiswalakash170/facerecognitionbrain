374532d81b43575a1469bdd00b0a52964f9196ff
Akash Jaiswal <akashjaiswal@iiitdmj.ac.in>
2021-07-27 09:35:21 +0530

    Added particles on background using react-tsparticles library

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import react, {Component} from 'react';
+import Particles from "react-tsparticles";
 import './App.css';
 import Navigation from './components/navigation/navigation';
 import Logo from './components/logo/logo';
 import ImageLinkForm from './components/imagelinkform/imagelinkform';
 import Rank from './components/rank/rank';
 import 'tachyons';
+import particleOptions from './particles.json';
+
 
 class App extends Component {
-  
+
     render() {
         return (
             <div className="App">
+                <Particles
+                    options={particleOptions}
+                />
                 <Navigation />
                 <Logo />
                 <Rank />
