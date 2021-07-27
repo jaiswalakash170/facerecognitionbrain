903748fc2828e8fa57fd7b8d152dc65a1adcc79a
Akash Jaiswal <akashjaiswal@iiitdmj.ac.in>
2021-07-27 20:55:19 +0530

    Added routing to home page on clicking the SignIn button

--- src/App.js
+++ src/App.js
@@ -56,6 +56,10 @@ class App extends Component {
             .catch(error => console.log(error));
     }
 
+    onRouteChange = () => {
+        this.setState({route: 'home'});
+    }
+
     render() {
         return (
             <div className="App">
@@ -64,7 +68,7 @@ class App extends Component {
                 />
                 <Navigation />
                 { this.state.route == 'signin' ?
-                    <SignIn />
+                    <SignIn onRouteChange={this.onRouteChange} />
                     :<div>
                         <Logo />
                         <Rank />
