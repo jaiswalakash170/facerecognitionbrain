1b21be903bdb2bfd8a1f54c818bff8fd8bc8301b
Akash Jaiswal <akashjaiswal@iiitdmj.ac.in>
2021-07-27 09:48:37 +0530

    Added onInputChange & onButtonSubmit methods in App and attached them to onChange & onClick events from ImageLinkForm component

--- src/App.js
+++ src/App.js
@@ -10,6 +10,20 @@ import particleOptions from './particles.json';
 
 
 class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            input: ''
+        }
+    }
+
+    onInputChange = (event) => {
+        console.log(event.target.value);
+    }
+
+    onSubmit = () => {
+        console.log("Click");
+    }
 
     render() {
         return (
@@ -20,7 +34,9 @@ class App extends Component {
                 <Navigation />
                 <Logo />
                 <Rank />
-                <ImageLinkForm />
+                <ImageLinkForm 
+                    onInputChange={this.onInputChange} 
+                    onButtonSubmit={this.onSubmit}/>
                 {/*
                 
                 <FaceRecognition />*/}
