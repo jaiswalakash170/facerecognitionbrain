1538434d0fcde8755286fd6a5adee601d417bcab
Akash Jaiswal <akashjaiswal@iiitdmj.ac.in>
2021-07-28 17:04:05 +0530

    Updated onSubmit method from App component to increment rank of the user on successful prediction

--- src/App.js
+++ src/App.js
@@ -76,8 +76,26 @@ class App extends Component {
 
     onSubmit = () => {
         this.setState({imageURL: this.state.input});
-        app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-            .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+        app.models
+            .predict(
+                Clarifai.FACE_DETECT_MODEL, 
+                this.state.input
+            )
+            .then(response => {
+                if(response){
+                    fetch('http://localhost:3001/image', {
+                            method: 'put',
+                            headers: {'Content-Type': 'application/json'},
+                            body: JSON.stringify({
+                                id: this.state.user.id
+                            })
+                        }
+                    )
+                    return this.displayFaceBox(this.calculateFaceLocation(response))
+                } else {
+                    throw new Error("Error from Clarifai API");
+                }
+            })
             .catch(error => console.log(error));
     }
 
